04978282b7fd8a7a05afe281e544122a2779d5e2
Zombie <158185295+ZombieGeeK0@users.noreply.github.com>
2024-02-22 15:28:16 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@ clear
 echo ====================================================================================================
 echo [?] Instalando...
 echo ====================================================================================================
-sudo apt-get update && sudo apt-get install git -y && sudo apt-get install python3 && sudo apt-get install python3-pip && pip3 install argparse
+sudo apt-get update && sudo apt-get install git -y && sudo apt-get install python3 && sudo apt-get install python3-pip && pip3 install argparse && sudo apt install -y tor torbrowser-launcher 
 echo ====================================================================================================
 echo [?] Requerimientos instalados.
