a0e2567bad5256ba3382b0542563fb79e243ed63
Zombie <158185295+ZombieGeeK0@users.noreply.github.com>
2024-02-22 10:51:30 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@ clear
 echo ====================================================================================================
 echo [?] Instalando...
 echo ====================================================================================================
-sudo su && sudo apt-get update && apt-get install git -y && sudo apt-get install python3 && sudo apt-get install python3-pip && pip3 install argparse
+sudo apt-get update && apt-get install git -y && sudo apt-get install python3 && sudo apt-get install python3-pip && pip3 install argparse
 echo ====================================================================================================
 echo [?] Requerimientos instalados.
